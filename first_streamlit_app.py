77eae6cdf9daa86c890dd19fb023faae3e448be9
snowflake-ninja <156745475+snowflake-ninja@users.noreply.github.com>
2024-01-16 00:59:28 -0500

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
 streamlit.title("🥣 Omega 3 & Blueberry Oatmeal")
@@ -6,4 +7,5 @@ streamlit.title("🥗 kale, spinach & Rocket Smoothie")
 streamlit.title("🐔 Hard-Boiled Free-Range Egg")
 streamlit.title("🥑🍞 Avocado Toast")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
