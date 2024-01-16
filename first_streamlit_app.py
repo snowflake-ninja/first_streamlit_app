48adcdafd15e2894599163b9068d75b2c289e02b
snowflake-ninja <156745475+snowflake-ninja@users.noreply.github.com>
2024-01-16 00:57:35 -0500

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
-streamlit.title("Omega 3 & Blueberry Oatmeal")
-streamlit.title("kale, spinach & Rocket Smoothie")
-streamlit.title("Hard-Boiled Free-Range Egg")
+streamlit.title("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.title("🥗 kale, spinach & Rocket Smoothie")
+streamlit.title("🐔 Hard-Boiled Free-Range Egg")
+streamlit.title("🥑🍞 Avocado Toast")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
